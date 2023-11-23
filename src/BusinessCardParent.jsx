74fb8dc37f3cbfa8af3eb915bbcb9aad4d0d8632
ConnorCoderAcademy <13716@coderacademy.edu.au>
2023-11-23 19:46:08 +1100

    Added BusinessCardParent+Display

---
+++ src/BusinessCardParent.jsx
@@ -0,0 +1,22 @@
+import { Component } from "react";
+import {BusinessCardDisplay} from "./BusinessCardDisplay";
+
+class BusinessCardParent extends Component {
+    constructor() {
+        super();
+    }
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+
+// import React from "react";
+
+// class BusinessCardParent extends React.Component {
+
+// }
