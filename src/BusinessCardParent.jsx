5e58bc0a149e38c410305c58bc114c0856189d89
ConnorCoderAcademy <13716@coderacademy.edu.au>
2023-11-25 10:13:05 +1100

    added per-property validation

--- src/BusinessCardParent.jsx
+++ src/BusinessCardParent.jsx
@@ -1,21 +1,48 @@
 import { Component } from "react"
 import BusinessCardDisplay from "./BusinessCardDisplay";
+import BusinessCardForm from "./BusinessCardForm";
 
 export default class BusinessCardParent extends Component {
     constructor(){
         super();
+
         this.state = {
             name: "Default Name",
-            email: "example@email.com"
+            email: "example@email.com",
+            editMode: false
+        }
+    }
+
+    updateState = (stateKeyId, newStateValue) => {
+        if (Object.keys(this.state).includes(stateKeyId)) {
+            if (stateKeyId == "name") {
+                // add name validation here
+            } else if (stateKeyId == "email") {
+                // add email validation here
+            }
+            
+            this.setState({
+                [stateKeyId]: newStateValue
+            })
+        } else {
+            console.warn("Incorrect key for the state")
         }
     }
 
     render(){
-        return(
-            <div>
+        if (this.state.editMode) {
+            return(
+                <BusinessCardForm 
+                    name={this.state.name} 
+                    email={this.state.email}
+                    updateState={this.updateState}
+                    />
+            )
+        } else {
+            return (
                 <BusinessCardDisplay name={this.state.name} email={this.state.email} />
-            </div>
-        )
+            )
+        }      
     }
 
 }
