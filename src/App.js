4b43ee023c74b516db56572028a1d493e3b402b5
ConnorCoderAcademy <13716@coderacademy.edu.au>
2023-11-25 12:12:01 +1100

    Navbar and navLinks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Navbar from './components/Navbar';
 import BusinessCardParent from './components/BusinessCardParent';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
@@ -9,6 +10,8 @@ import { Route, Routes, Outlet } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
+      <h1>This is shown in all the pages</h1>
+      <Navbar/>
       <Routes>
         <Route path='/' element={<HomePage />}></Route>
         {/* <Route path='/about' element={<AboutPage />} />
@@ -18,8 +21,8 @@ function App() {
         <Route path="/about" element={<Outlet />}> 
           <Route index element= {<AboutPage />} />
           <Route path="contact" element={<ContactPage/>}></Route>
-
         </Route>
+        <Route path='/cards' element={<BusinessCardPage />} />
 
 
       </Routes>
